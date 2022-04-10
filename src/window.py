51a995f0e1ec5a946a8d78e3874eba19465d5c1f
Hilmanda Panji Orienski <hilmanda73@gmail.com>
2022-04-11 05:30:45 +0700

    fix icon path and recompile

--- src/window.py
+++ src/window.py
@@ -1,12 +1,14 @@
 from tkinter import *
 from tkinter.messagebox import showerror
+from os import path
 
 class Window(Tk):
     #create and configure window
     def __init__(self):
         super().__init__()
         self.title('PanjiEditor - Untitled')
-        self.iconphoto(True, PhotoImage(file='/PanjiEditor/src/icons/favicon-32.png'))
+        imgpath = path.dirname(__file__)
+        self.iconphoto(True, PhotoImage(file= imgpath + '/icons/favicon-32.png'))
         self.rowconfigure(0, weight=1)
         self.columnconfigure(1, minsize=800, weight=1)
         self.minsize(height=800, width=885)
